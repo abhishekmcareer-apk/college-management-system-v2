c23c321931946643da7ed99932ff21416a1a6344
abhishekmcareer-apk <abhishekm.career@gmail.com>
2026-09-03 12:47:08 +0530

    Add socket chat feature and update footer logout

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,19 +1,45 @@
+import { configDotenv } from "dotenv";
 import app from "./app.js"
-import dotenv from "dotenv"
+import http from "http"
+import { Server } from "socket.io";
 import connectDB from "./config/db.js"
 
-
-dotenv.config()
+configDotenv()
 
 const PORT = process.env.PORT || 5000
 
 const startServer = async () => {
     await connectDB()
 
-   
-    app.listen(PORT,()=>{
-        console.log(`Server Started at PORT ${PORT}`)
+    const server = http.createServer(app)
+
+    const io = new Server(server, {
+        cors: {
+            origin: [
+                "https://college-management-frontend-v2.onrender.com",
+                "http://localhost:5173"
+            ],
+            credentials: true
+        }
     })
+
+
+    io.on("connection", (socket) => {
+        console.log("New user added", socket.id)
+
+        socket.on("send_message", (data) => {
+            io.emit("receive_message", data)
+        })
+
+        socket.on("disconnect", () => {
+            console.log("User disconnected", socket.id)
+        })
+    })
+
+    server.listen(PORT,()=>{
+        console.log(`Server started at ${PORT} with socket.io`)
+    })
+
 }
 
 startServer()
